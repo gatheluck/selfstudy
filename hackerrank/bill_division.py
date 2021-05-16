b625c488c95d7016b0283e435e98e210d3a51601
gatheluck <gatheluck+tech@gmail.com>
2021-05-17 08:25:00 +0900

    feat: add bill_division.py

---
+++ hackerrank/bill_division.py
@@ -0,0 +1,38 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'bonAppetit' function below.
+#
+# The function accepts following parameters:
+#  1. INTEGER_ARRAY bill
+#  2. INTEGER k
+#  3. INTEGER b
+#
+
+def bonAppetit(bill, k, b):
+    # Write your code here
+    actual_bill: int = (sum(bill) - bill[k]) / 2
+    bill_difference: int = int(b - actual_bill)
+    if bill_difference == 0:
+        print("Bon Appetit")
+    else:
+        print(bill_difference)
+
+if __name__ == '__main__':
+    first_multiple_input = input().rstrip().split()
+
+    n = int(first_multiple_input[0])
+
+    k = int(first_multiple_input[1])
+
+    bill = list(map(int, input().rstrip().split()))
+
+    b = int(input().strip())
+
+    bonAppetit(bill, k, b)
